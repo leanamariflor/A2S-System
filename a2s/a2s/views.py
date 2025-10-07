0dc29e1074b29d231093ff8335f3482cb45dda4a
Jaylord Bayonas <jaylordbayonas87@gmail.com>
2025-10-07 23:47:21 +0800

    Edit Profile Function

--- a2s/a2s/views.py
+++ a2s/a2s/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from a2s_system.models import StudentProfile
 
 @login_required
 def student_dashboard(request):
+    
+    student_profile = None
+    if hasattr(request.user, 'studentprofile'):
+        student_profile = request.user.studentprofile
+
     return render(request, "dashboard.html", {
-        "user": request.user
+        "user": request.user,
+        "student_profile": student_profile
     })
